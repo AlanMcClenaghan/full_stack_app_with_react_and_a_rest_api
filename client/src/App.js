e6f4be95af1fe04e03a8de05d8080e75222f63dd
Alan McClenaghan <AlanM@Alans-MBP-2.cust.communityfibre.co.uk>
2024-03-11 00:06:27 +0000

    Test calling your REST API from your React application

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/api/courses")
+    .then(response => response.json())
+    .then(responseData => setCourses(responseData))
+    .catch(error => console.log("Error fetching and parsing date", error));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {courses.map(course => (
+        <li key={course.id}>{course.title}</li>
+      ))}
+    </ul>
   );
 }
 
